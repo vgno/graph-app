e72d4457f0d70700b9685f40fc6c0cd45e9b0088
Kristoffer Brabrand <kristoffer@brabrand.no>
2014-08-08 21:59:22 +0200

    Fixed topic filtering. Named param seemed to mess up types somehow. For now fixed by doing implode in query instead.

--- library/Graph/Controllers/RecommendationController.php
+++ library/Graph/Controllers/RecommendationController.php
@@ -18,29 +18,29 @@ class RecommendationController {
 
         $articleId = (int) $articleId;
         $limit     = 10;
-        $topics    = false;
+        $topics    = isset($_GET['topics']) ? $_GET['topics'] : [];
 
         if (isset($_GET['limit']) && is_numeric($_GET['limit']) && $_GET['limit'] < 100) {
             $limit = (int) $_GET['limit'];
         }
 
-        if (isset($_GET['topics']) && is_array($_GET['topics'])) {
-            $topics = array_filter(array_map(function($val) {
-                if (!is_numeric($val)) {
-                    return false;
-                }
+        $topics = is_string($topics) ? explode(',', $topics) : $topics;
+        $topics = array_filter(array_map(function($val) {
+            if (!is_numeric($val)) {
+                return false;
+            }
 
-                return intval($val);
-            }, $_GET['topics']));
-        }
+            return intval($val);
+        }, $topics));
 
         // Filtering on topics
         if (count($topics)) {
             $recommended = new Query(
                 $app['neo4j'],
-                'MATCH (article:article)<-[:read]-(user:ip)-[:read]->(recommended:article)<-[:listed]-(topic:topic {topicId:1909})
+                'MATCH (article:article)<-[:read]-(user:ip)-[:read]->(recommended:article)<-[:listed]-(topic:topic)
                 WHERE
                     article.articleId = {articleId}
+                    AND topic.topicId IN [' . implode(',', $topics) . ']
                 RETURN
                     recommended.articleId as articleId,
                     recommended.title as title,
@@ -49,8 +49,7 @@ class RecommendationController {
                 LIMIT {limit}',
                 [
                     'articleId' => $articleId,
-                    'limit'     => $limit,
-                    'topics'    => sprintf('[%s]', implode(',', $topics))
+                    'limit'     => $limit
                 ]
             );
         } else {
