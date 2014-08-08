49c3e1a08e1b315a52fd1b1aee6273d856b2fbad
Kristoffer Brabrand <kristoffer@brabrand.no>
2014-08-08 13:16:27 +0200

    Implemented jsonp support

--- library/Graph/Application.php
+++ library/Graph/Application.php
@@ -4,6 +4,10 @@ namespace Graph;
 
 use KBrabrand\Silex\Provider\Neo4jServiceProvider;
 
+use Symfony\Component\HttpFoundation\JsonResponse,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response;
+
 class Application extends \Silex\Application {
     public function __construct(array $config = array()) {
         parent::__construct();
@@ -17,6 +21,18 @@ class Application extends \Silex\Application {
             'neo4j.transport' => 'vg-neo4j-01',
             'neo4j.port'      => 7474,
         ));
+
+        $this->after(function(Request $req, Response $res) {
+            $callback = $req->get('callback');
+
+            if ($callback !== null && $req->getMethod() === 'GET') {
+                if ($res instanceof JsonResponse) {
+                    $res->setCallBack($callback);
+                } else {
+                    $res->setContent($callback . '(' . $res->getContent() . ');');
+                }
+            }
+        });
 
         $this->get('/recommended/{articleId}',  'Graph\Controllers\RecommendationController::thoseWhoReadalsoReadAction');
 
