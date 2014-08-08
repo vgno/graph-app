a83a6e9c18dc9fda4dd734b2ba38a6c53e2080c4
Kristoffer Brabrand <kristoffer@brabrand.no>
2014-08-08 16:04:30 +0200

    Added topic filter

--- library/Graph/Controllers/RecommendationController.php
+++ library/Graph/Controllers/RecommendationController.php
@@ -18,26 +18,59 @@ class RecommendationController {
 
         $articleId = (int) $articleId;
         $limit     = 10;
+        $topics    = false;
 
         if (isset($_GET['limit']) && is_numeric($_GET['limit']) && $_GET['limit'] < 100) {
             $limit = (int) $_GET['limit'];
         }
 
-        $recommended = new Query(
-            $app['neo4j'],
-            'MATCH (article:article)<-[:read]-(user:ip)-[:read]->(recommended:article)
-            WHERE article.articleId = {articleId}
-            RETURN
-                recommended.articleId as articleId,
-                recommended.title as title,
-                count(*) as reads
-            ORDER BY reads DESC
-            LIMIT {limit}',
-            [
-                'articleId' => $articleId,
-                'limit'     => $limit
-            ]
-        );
+        if (isset($_GET['topics']) && is_array($_GET['topics'])) {
+            $topics = array_filter(array_map(function($val) {
+                if (!is_numeric($val)) {
+                    return false;
+                }
+
+                return intval($val);
+            }, $_GET['topics']));
+        }
+
+        // Filtering on topics
+        if (count($topics)) {
+            $recommended = new Query(
+                $app['neo4j'],
+                'MATCH (article:article)<-[:read]-(user:ip)-[:read]->(recommended:article)<-[:listed]-(topic:topic {topicId:1909})
+                WHERE
+                    article.articleId = {articleId}
+                RETURN
+                    recommended.articleId as articleId,
+                    recommended.title as title,
+                    count(*) as reads
+                ORDER BY reads DESC
+                LIMIT {limit}',
+                [
+                    'articleId' => $articleId,
+                    'limit'     => $limit,
+                    'topics'    => sprintf('[%s]', implode(',', $topics))
+                ]
+            );
+        } else {
+            $recommended = new Query(
+                $app['neo4j'],
+                'MATCH (article:article)<-[:read]-(user:ip)-[:read]->(recommended:article)
+                WHERE
+                    article.articleId = {articleId}
+                RETURN
+                    recommended.articleId as articleId,
+                    recommended.title as title,
+                    count(*) as reads
+                ORDER BY reads DESC
+                LIMIT {limit}',
+                [
+                    'articleId' => $articleId,
+                    'limit'     => $limit
+                ]
+            );
+        }
 
         $result = [];
 
